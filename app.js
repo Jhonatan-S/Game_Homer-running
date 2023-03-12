9b31a50af148d000b28956c0132f934278865be8
Jhonatan-S <studyprogrammer5@gmail.com>
2023-03-12 01:27:00 -0300

    Upgrade
    Adicionei o Mário toda vez que for aumentar a velocidade e também um efeito sonoro de bomba.
    Coloquei também uma função para que o título seja escrito de forma digitada

--- app.js
+++ app.js
@@ -1,39 +1,70 @@
-; // Quando qualquer tecla for digitada a função jump será chamada
+// Guardando o elementos em constantes
+const title = document.querySelector('.title')
+const pipe = document.querySelector('.pipe'); // doto do cano
+const score = document.querySelector('#score'); // O contador de pontos
+const audio_jump = document.querySelector('.audio-jump'); // Som quando o homer pula
+const audio_lose = document.querySelector('.audio-lose'); // Som quando o homer bate no cano
+const game_over = document.querySelector('.game_over'); // O texto game over
+const mario_aplaudindo = document.querySelector('.mario_aplaudindo')
+const fumaca = document.querySelector('.fumaca')
+const audio_bomba = document.querySelector('.bomba_marioAplaudindo')
+const aumentar_velocidade = document.querySelector('.aumentar_velocidade')
+let count_score = 0; // score
+let time_score = 250; // Determinar o tempo em ms do contador de ponto
+
+
+function escrever(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 150 * i)
+
+  });
+}
+const titulo = document.querySelector('.title');
+escrever(titulo);
 
 function start() {
     document.querySelector('.dormindo').classList.add('running')
     document.querySelector('.running').classList.add('homer')
     document.querySelector('.homer').setAttribute('src', 'Image/homer/running.gif')
     document.querySelector('.running').classList.remove('dormindo')
-
-    document.querySelector('.title').style.fontSize = '1rem'
-    document.querySelector('.title').style.textAlign = 'left'
-    document.querySelector('.title').style.top = '0'
-    document.querySelector('.title').style.textShadow = '1px 1px #fff'
-
+    title.style.fontSize = '1rem'
+    title.style.top = '0'
+    title.style.textShadow = '1px 1px #fff'
+    document.querySelector('.press_start').style.visibility = 'hidden'
     score_count()
 }
 
-// Guardando o elemento pipe, homer e score
-const pipe = document.querySelector('.pipe');
-const score = document.querySelector('#score');
-const audio_jump = document.querySelector('.audio-jump');
-const audio_lose = document.querySelector('.audio-lose');
-const game_over = document.querySelector('.game_over');
-let count = 0;
-let time_score = 250;   
-
+// Se a tecla seleciona for seta pra cima a função jump será chamada
 
 document.addEventListener('keydown', (e) =>{
     if (e.key === 'ArrowUp') {
-        jump()
+
+        // Se a frase game over ou press start estiverem na tela 
+        if (game_over.style.visibility) {
+            audio_jump.pause()
+
+        }else{
+            audio_jump.play()
+            document.querySelector('.running').classList.add('jump');
+        }
+    
+        setTimeout( () => {
+            document.querySelector('.running').classList.remove('jump')
+    
+        } , 510);
     }
 })
 
+// Se a tecla pressionada for seta pra baixo o homer vai embaixar 
 document.addEventListener('keydown', (e) =>{
-    
     if (e.key === 'ArrowDown'){
-
+        
+        // Se a frase game over não esitver visível o homer vai abaixar. Condição feita para ele não ficar recebendo esse comando caso tiver batido no cano
         if (game_over.style.visibility == false) {
             document.querySelector('.running').setAttribute('src', 'Image/homer/abaixado.gif')
             document.querySelector('.running').classList.add('abaixado')
@@ -42,10 +73,13 @@ document.addEventListener('keydown', (e) =>{
     }
 })
 
+
+
+// Se a tecla seta para baixo for solta o homer voltará a ficar em pé
 document.addEventListener('keyup', (event) => {
 
     if (event.key === 'ArrowDown') {
-
+        // Se a frase game over não esitver visível o homer vai levantar. Condição feita para ele não ficar recebendo esse comando caso tiver batido no cano
         if (game_over.style.visibility == false) {
 
             document.querySelector('.abaixado').setAttribute('src', 'Image/homer/running.gif')
@@ -56,34 +90,16 @@ document.addEventListener('keyup', (event) => {
     }
 })
 
-
-function jump() {
-
-    // Criando uma condição. Se o h1 gameover estiver visível o efeito de pular será pausado
-    if (game_over.style.visibility) {
-        audio_jump.pause()
-    }else{
-        audio_jump.play()
-    }
-
-    document.querySelector('.running').classList.add('jump');
-
-    setTimeout( () => {
-        document.querySelector('.running').classList.remove('jump')
-
-    } , 510);
-}
-
 function score_count() {
-    setInterval( () => {
+    const loop_score = setInterval( () => {
         
         // Criando condição. Se o homer bater no pipe (feito através de localização) troca a imagem do homer e para o loop
     
         if (game_over.style.visibility) {
             clearInterval(loop_score)
         }else{
-            count++;
-            score.innerHTML = 'SCORE:' + count
+            count_score++;
+            score.innerHTML = 'SCORE:' + count_score
         }
     
     }, time_score ); // 10 mili segundos
@@ -106,7 +122,6 @@ const loop = setInterval( () => {
         pipe.style.animation = 'none';
         pipe.style.left = positionPipe + 'px';
         
-        homer.style.animation = 'none';
         homer.style.bottom = homerPosition + 'px';
 
         const homer_death = 'Image/homer/lose.gif'
@@ -117,32 +132,69 @@ const loop = setInterval( () => {
         game_over.style.visibility = 'visible'
 
         document.querySelector('h2').style.visibility = 'visible'
-        document.querySelector('h2').innerHTML = 'SCORE:' + count
+        document.querySelector('h2').innerHTML = 'SCORE:' + count_score
         document.querySelector('.box_score').style.visibility = 'hidden'
 
         clearInterval(loop) // Parando o loop após o homer bater no pipe
-    }
-    else if (count > 100 && count < 200) {
-        if (positionPipe < 10) {
-            if (positionPipe > 0) {
-                pipe.style.animationDuration = '2s'
-            }
-        } 
-    }else if (count > 200 && count < 300) {
-        if (positionPipe < 10) {
-            if (positionPipe > 0) {
-                pipe.style.animationDuration = '1s'
-            }
-        } 
-    }else if (count > 300 && count < 400) {
-        if (positionPipe < 10) {
-            if (positionPipe > 0) {
-                pipe.style.animationDuration = '0.5s'
-            }
-        } 
-    }
-    else if (count == 400) {
-        
-    }
 
+    }
+    else if (count_score == 10) { 
+
+        pipe.style.animationDuration = '2s'
+        pipe.style.visibility = 'hidden'
+        pipe.style.animationName = 'none'
+        mario()
+        
+    }else if (count_score == 100) {
+        
+        pipe.style.animationDuration = '1.5s'
+        pipe.style.visibility = 'hidden'
+        pipe.style.animationName = 'none'
+        mario()
+             
+        
+    }else if (count_score == 200) {
+    
+        pipe.style.animationDuration = '1s'
+        pipe.style.visibility = 'hidden'
+        pipe.style.animationName = 'none'
+        mario()
+            
+    } 
+    
 }, 10); // 10 mili segundos
+
+
+function mario() {
+
+    setTimeout ( () => {
+        aumentar_velocidade.style.visibility = 'visible'
+        mario_aplaudindo.style.visibility = 'visible'
+        audio_bomba.play()
+        fumaca.style.visibility = 'visible'
+    },500)
+
+    let contador = 5
+    let opacidade = 1.5
+
+    while(contador > 0){
+
+            if (contador > 0) {
+        
+                fumaca.style.opacity = opacidade
+                audio_bomba.pause()
+                contador --
+                opacidade -= 0.3
+            }
+        }
+                
+    setTimeout( () => {   
+
+            aumentar_velocidade.style.visibility = 'hidden'
+            fumaca.style.visibility = 'hidden'
+            mario_aplaudindo.style.visibility = 'hidden'
+            pipe.style.visibility = 'visible'
+            pipe.style.animationName = 'pipe-animation'
+            contador = 1.5
+    }, 5000)
+}
